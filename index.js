3a25acad6cadc855d50b6735939e4be63d4c9de9
TheMonist <rogerrosena@gmail.com>
2024-08-10 15:38:05 -0400

    working on html files

--- index.js
+++ index.js
@@ -3,40 +3,28 @@ const url = require('url');
 const fs = require('fs');
 const PORT = 8080;
 
+const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+    if (err) throw err;
+    return data;
+})
+
 const server = http.createServer(function(req, res){
     const q = url.parse(req.url, true);
     const filename = req.url === "/" ? "./index.html":`./${q.pathname}.html`;
-    const fileNotFount = './404';
 
     fs.readFile(filename, (err, data) => {
         if (err) {
           res.writeHead(404, {'Content-Type': 'text/html'});
-          return res.end("404 Not Found");
+          res.write(page404);
+          return res.end();
         } else {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
             return res.end();
         }
-        
       });
 });
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
-
-/* 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
-*/
